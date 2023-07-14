e755b3c955a2d9f02730161632e562694acdfe70
Samimi <smokhadar@gmail.com>
2023-07-14 12:16:36 -0700

    trying to fix partial bug

--- server.js
+++ server.js
@@ -11,23 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 const hbs = exphbs.create({ helpers });
-
-app.set('view engine', 'handlebars');
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.engine('handlebars', hbs.engine({
-  layoutsDir: __dirname + "/views/layouts",
-  partialsDir: __dirname + "/views/partials",
-}));
-
-const navbar = exphbs.compile(fs.readFileSync(__dirname + '/views/partials/navbar.handlebars').toString('utf-8'));
-exphbs.registerPartial('myPartial', navbar);
-
-app.use(router);
 
 const sess = {
   secret: 'Super secret secret',
@@ -45,6 +29,19 @@ const sess = {
 };
 
 app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+
+app.use(router);
+
+const navbar = hbs._compileTemplate(fs.readFileSync(__dirname + '/views/partials/navbar.handlebars').toString('utf-8'));
+hbs.getPartials('navBar', navbar);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
